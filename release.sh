7b4beca01497cb96202d64bf195a60cf1ce09eac
Francisco Riadigos <riadigos.fran@gmail.com>
2018-05-08 16:52:19 +0100

    Added main branch where to push to

--- release.sh
+++ release.sh
@@ -4,15 +4,16 @@ set -e
 # debug log
 set -x
 
-if [ $# -lt 4 ]; then
-    echo "You need to provide 4 arguments"
+if [ $# -lt 5 ]; then
+    echo "You need to provide 5 arguments"
     exit 1
 fi
 
-version_tag_prefix=$1
-final_version=$2
-mapping_path=$3
-version_upgrade_message=$4
+main_branch=$1
+version_tag_prefix=$2
+final_version=$3
+mapping_path=$4
+version_upgrade_message=$5
 
 IFS=\. read major minor patch <<< "$final_version"
 
@@ -26,4 +27,4 @@ git status
 git add .
 git commit -m "<$version_tag_prefix$final_version> $version_upgrade_message"
 git tag "$version_tag_prefix$final_version"
-git push --tags origin HEAD
+git push --tags origin HEAD:${main_branch}
