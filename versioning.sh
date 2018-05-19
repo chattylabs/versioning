7fb522fb10de9acb596543cdf382953d02036787
Francisco Riadigos <riadigos.fran@gmail.com>
2018-05-19 11:21:34 +0100

    Extracted update version.properties

--- versioning.sh
+++ versioning.sh
@@ -5,11 +5,9 @@ set -e
 set -x
 
 if [ $# -lt 3 ]; then
-    echo "You need to provide 3 arguments"
-    exit 1
+  echo "You need to provide 3 arguments"
+  exit 1
 fi
-
-DEFAULT_VERSION="0.1.0"
 
 version_prefix=$1
 feature_commit_pattern=$2
@@ -30,9 +28,10 @@ if [[ ${project} =~ .*$version_prefix[0-99]+\.[0-99]+\.[0-99]+.* ]]; then
   original_version=${original_version##"$version_prefix"}
   original_version=${original_version%%-*}
 fi
+
 if [[ -z "$original_version" ]]; then
-  original_version=${DEFAULT_VERSION}
-  is_new=true
+  echo "You need to create a version tag first"
+  exit 1
 else
   last_version_commit_id=`git rev-list -n 1 "$version_prefix$original_version"`
 fi
@@ -63,8 +62,5 @@ minor=$(($minor + $number_of_features))
 patch=$(($patch + $number_of_patches))
 
 final_version="$major.$minor.$patch"
-
-./gradlew doUpdateVersion -Pkey=minor -Pvalue=${minor} --quiet &> /dev/null
-./gradlew doUpdateVersion -Pkey=patch -Pvalue=${patch} --quiet &> /dev/null
 
 echo "$final_version|$original_version"
